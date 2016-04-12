f9b779439fcbb032f9c1824be2108c1fee5f7e47
toon1992 <toon.detrue.v7134@hogent.be>
2016-04-12 12:53:57 +0200

    Merge origin/master
    Conflicts:
    	src/gui/ReservatieDetails.fxml

--- src/domein/Beheerder.java
+++ src/domein/Beheerder.java
@@ -5,16 +5,11 @@
  */
 package domein;
 
-import java.io.File;
-import java.util.List;
-import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
