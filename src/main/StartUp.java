42c39a1566385659879c32520fbff94002f6b0be
Donovan De Smedt <donovandesmedt@live.be>
2016-04-20 17:52:17 +0200

    Testen aanpassen statussen

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -28,7 +28,7 @@ public class StartUp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-       // initializeDatabase();
+       initializeDatabase();
         Scene scene = new Scene(new LayoutFrameController());
         primaryStage.setMaximized(true);
         primaryStage.setScene(scene);
@@ -47,7 +47,7 @@ public class StartUp extends Application {
         GenericDaoJpa<Object> jpa = new GenericDaoJpa<>(Object.class);
         jpa.startTransaction();
 
-        jpa.insert(new Beheerder("admin@hogent.be", "admin", "admin",true));
+        jpa.insert(new Beheerder("admin@hogent.be", "admin", "admin", true));
 
         Firma globe = new Firma("Globe atmosphere", "globe@atmosphere.com");
         Firma prisma = new Firma("Prisma", "helpdesk@prisma.com");
@@ -89,23 +89,23 @@ public class StartUp extends Application {
         jpa.insert(maatschappij);
         jpa.insert(mens);
 //
-        Materiaal wereldbol = new Materiaal("C:\\Users\\ToonDT\\Desktop\\School 2015-2016\\Projecten\\Java\\groep6\\src\\images\\wereldbol.png", "Wereldbol", "Globe met verlichting, boldoorsnede 26cm", "B2.13", 1234, 4, 0, 26.56, true, globe, new HashSet<>(Arrays.asList(lager, secundair)), new HashSet<>(Arrays.asList(aardrijkskunde,maatschappij,mens,geschiedenis)));
+        Materiaal wereldbol = new Materiaal("/Users/donovandesmedt/NetBeansProjects/groep06Java/groep06Java/src/images/wereldbol.png", "Wereldbol", "Globe met verlichting, boldoorsnede 26cm", "B2.13", 1234, 4, 0, 26.56, true, globe, new HashSet<>(Arrays.asList(lager, secundair)), new HashSet<>(Arrays.asList(aardrijkskunde,maatschappij,mens,geschiedenis)));
 
         jpa.insert(wereldbol);
 
-        Materiaal rekenMachine = new Materiaal("C:\\Users\\ToonDT\\Desktop\\School 2015-2016\\Projecten\\Java\\groep6\\src\\images\\texas.jpg", "TI 84 plus", "Grafisch rekentoestel van Texas instrument", "B3.43", 2345, 10, 1, 116.99, true, texas, new HashSet<>(Arrays.asList(secundair)), new HashSet<>(Arrays.asList(wiskunde, fysica, techniek)));
+        Materiaal rekenMachine = new Materiaal("/Users/donovandesmedt/NetBeansProjects/groep06Java/groep06Java/src/images/texas.jpg", "TI 84 plus", "Grafisch rekentoestel van Texas instrument", "B3.43", 2345, 10, 1, 116.99, true, texas, new HashSet<>(Arrays.asList(secundair)), new HashSet<>(Arrays.asList(wiskunde, fysica, techniek)));
 
         jpa.insert(rekenMachine);
 //
-        jpa.insert(new Materiaal("C:\\Users\\ToonDT\\Desktop\\School 2015-2016\\Projecten\\Java\\groep6\\src\\images\\erlenmeyer.jpg", "Erlenmeyer", "geschiedenis pack contains one flask each of 50, 125, 250, 500, and 1000mL sizes.", "B1.00", 3445, 20, 0, 43.55, false, kimax, new HashSet<Doelgroep>(Arrays.asList(secundair)), new HashSet<Leergebied>(Arrays.asList(wetenschap,biologie))));
-        jpa.insert(new Materiaal("C:\\Users\\ToonDT\\Desktop\\School 2015-2016\\Projecten\\Java\\groep6\\src\\images\\prisma.jpg", "Prisma Duits-Nederlands", "Pocketwoordenboek Duits-Nederlands Prisma", "B2.13", 4566, 22, 0, 9.50, true, prisma, new HashSet<Doelgroep>(Arrays.asList(lager, secundair)), new HashSet<Leergebied>(Arrays.asList(mens,maatschappij))));
-        jpa.insert(new Materiaal("C:\\Users\\ToonDT\\Desktop\\School 2015-2016\\Projecten\\Java\\groep6\\src\\images\\geo.jpg", "Bordgeodriehoek", "Bordgeodriehoek Wissner 80cm", "B2.13", 5431, 4, 0, 26.15, true, wissner, new HashSet<Doelgroep>(Arrays.asList(lager, secundair)), new HashSet<Leergebied>(Arrays.asList(wiskunde, fysica, techniek))));
+        jpa.insert(new Materiaal("/Users/donovandesmedt/NetBeansProjects/groep06Java/groep06Java/src/images/erlenmeyer.jpg", "Erlenmeyer", "geschiedenis pack contains one flask each of 50, 125, 250, 500, and 1000mL sizes.", "B1.00", 3445, 20, 0, 43.55, false, kimax, new HashSet<Doelgroep>(Arrays.asList(secundair)), new HashSet<Leergebied>(Arrays.asList(wetenschap,biologie))));
+        jpa.insert(new Materiaal("/Users/donovandesmedt/NetBeansProjects/groep06Java/groep06Java/src/images/prisma.jpg", "Prisma Duits-Nederlands", "Pocketwoordenboek Duits-Nederlands Prisma", "B2.13", 4566, 22, 0, 9.50, true, prisma, new HashSet<Doelgroep>(Arrays.asList(lager, secundair)), new HashSet<Leergebied>(Arrays.asList(mens,maatschappij))));
+        jpa.insert(new Materiaal("/Users/donovandesmedt/NetBeansProjects/groep06Java/groep06Java/src/images/geo.jpg", "Bordgeodriehoek", "Bordgeodriehoek Wissner 80cm", "B2.13", 5431, 4, 0, 26.15, true, wissner, new HashSet<Doelgroep>(Arrays.asList(lager, secundair)), new HashSet<Leergebied>(Arrays.asList(wiskunde, fysica, techniek))));
 
         jpa.insert(new Gebruiker("Lector", "lector@hogent.be", "LE"));
-        jpa.insert(new Reservatie(2,2,0,new Date(116, 3, 11), new Date(116,3,15),new Date(),new HashSet<Dag>(),ReservatieStateEnum.Gereserveerd,new Gebruiker("Toon","toondetrue@gmail.com","ST"),wereldbol));
-        jpa.insert(new Reservatie(1,1,0,new Date(116, 3, 11), new Date(116,3,15),new Date(),new HashSet<Dag>(),ReservatieStateEnum.Gereserveerd,new Gebruiker("Donovan","donovandesmedt@gmail.com","ST"),wereldbol));
-        jpa.insert(new Reservatie(2,2,0,new Date(116, 2, 23), new Date(116,3,6),new Date(),new HashSet<Dag>(),ReservatieStateEnum.Gereserveerd,new Gebruiker("Manu","manuschoenmakers@gmail.com","ST"),rekenMachine));
-        jpa.insert(new Reservatie(3,3,0,new Date(116, 3, 23), new Date(116,4,17),new Date(),new HashSet<Dag>(), ReservatieStateEnum.Gereserveerd,new Gebruiker("Thomas","thomasledoux@gmail.com","ST"),rekenMachine));
+        jpa.insert(new Reservatie(2,0,0,new Date(116, 3, 11), new Date(116,3,15),new Date(),new HashSet<Dag>(),ReservatieStateEnum.Gereserveerd,new Gebruiker("Toon","toondetrue@gmail.com","ST"),wereldbol));
+        jpa.insert(new Reservatie(1,0,0,new Date(116, 3, 11), new Date(116,3,15),new Date(),new HashSet<Dag>(),ReservatieStateEnum.Gereserveerd,new Gebruiker("Donovan","donovandesmedt@gmail.com","ST"),wereldbol));
+        //jpa.insert(new Reservatie(2,new Date(116, 2, 23), new Date(116,3,6),new Date(),ReservatieStateEnum.Gereserveerd,new Gebruiker("Manu","manuschoenmakers@gmail.com","ST"),rekenMachine));
+        //jpa.insert(new Reservatie(3,new Date(116, 3, 23), new Date(116,4,6),new Date(),ReservatieStateEnum.Gereserveerd,new Gebruiker("Thomas","thomasledoux@gmail.com","ST"),rekenMachine));
 
         jpa.commitTransaction();
 
