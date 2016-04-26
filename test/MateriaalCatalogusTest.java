0302472d1e0e108ff77131994f7c18d432e827d4
thomasledoux1 <thomas.ledoux.q1069@student.hogent.be>
2016-04-26 20:47:49 +0200

    Wijzigen aan testen materiaalCatalogus

--- test/MateriaalCatalogusTest.java
+++ test/MateriaalCatalogusTest.java
@@ -17,6 +17,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import java.util.*;
+import static org.mockito.Matchers.anyString;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import persistentie.FirmaDao;
 import persistentie.MateriaalDao;
 
@@ -25,7 +28,7 @@ import persistentie.MateriaalDao;
  * @author donovandesmedt
  */
 public class MateriaalCatalogusTest {
-   
+
     @Mock
     private MateriaalDao materiaalDao;
     @Mock
@@ -35,92 +38,89 @@ public class MateriaalCatalogusTest {
     private MateriaalData materiaalData;
 
     @Before
-    public void before(){
+    public void before() {
         MockitoAnnotations.initMocks(this);
         materiaalData = new MateriaalData();
         trainDummy();
-        catalogus = new MateriaalCatalogus();
+        catalogus = new MateriaalCatalogus(materiaalDao, firmaDao);
     }
 
     public void trainDummy() {
         Mockito.when(materiaalDao.findAll()).thenReturn(materiaalData.geefMaterialen());
         Mockito.when(firmaDao.findAll()).thenReturn(materiaalData.geefFirmas());
+        Mockito.when(materiaalDao.getMaterialen()).thenReturn(materiaalData.geefMaterialen());
     }
 
     @Test
-    public void geefFirmaGeeftJuisteFirmaTerug()
-    {
+    public void geefFirmaGeeftJuisteFirmaTerug() {
         Firma f = catalogus.geefFirma("Prisma", "helpdesk@prisma.com");
         Assert.assertEquals("Prisma", f.getNaam());
         Assert.assertEquals("helpdesk@prisma.com", f.getEmailContact());
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
-    public void geefFoutAlsMateriaalBestaatMetNaam()
-    {
+    public void geefFoutAlsMateriaalBestaatMetNaam() {
         materialen = catalogus.geefMaterialen();
-        catalogus.controleerUniekheidMateriaalnaam("wereldbol");
+        catalogus.controleerUniekheidMateriaalnaam("Wereldbol");
     }
-    
+
     @Test
-    public void verwijderMateriaalTest()
-    {
+    public void verwijderMateriaalTest() {
         Materiaal m = materiaalData.getMateriaalRekenmachine();
         catalogus.verwijderMateriaal(m);
         materialen = catalogus.geefMaterialen();
         Assert.assertEquals(1, materialen.size());
     }
-    
+
     @Test
-    public void testZoekMateriaalCorrecteZoektermGeeftMateriaalTerug(){
+    public void testZoekMateriaalCorrecteZoektermGeeftMateriaalTerug() {
         materialen = catalogus.geefMaterialen();
         Set<String> zoekTermen = new HashSet<>();
         zoekTermen.add("Wereldbol");
         catalogus.zoek(zoekTermen);
         Assert.assertEquals(1, materialen.size());
     }
-    
+
     @Test
-    public void testZoekMateriaalParameterNullGeeftAlleMaterialenTerug(){
+    public void testZoekMateriaalParameterNullGeeftAlleMaterialenTerug() {
         materialen = catalogus.geefMaterialen();
         catalogus.zoek(null);
         Assert.assertEquals(2, materialen.size());
     }
-    
+
     @Test
-    public void testZoekMateriaalLegeSetGeeftAlleMaterialenTerug(){
+    public void testZoekMateriaalLegeSetGeeftAlleMaterialenTerug() {
         materialen = catalogus.geefMaterialen();
         catalogus.zoek(new HashSet<>());
         Assert.assertEquals(2, materialen.size());
     }
-    
+
     @Test
-    public void testZoekMateriaalOnbestaandeZoektermGeeftGeenMaterialenTerug(){
+    public void testZoekMateriaalOnbestaandeZoektermGeeftGeenMaterialenTerug() {
         materialen = catalogus.geefMaterialen();
         Set<String> zoekTermen = new HashSet<>();
         zoekTermen.add("blablabla");
         catalogus.zoek(zoekTermen);
         Assert.assertEquals(0, materialen.size());
     }
-    
+
     @Test
-    public void testFilterMateriaalMetEenZoektermGeeftAlleMaterialenTerugVanSoort(){
+    public void testFilterMateriaalMetEenZoektermGeeftAlleMaterialenTerugVanSoort() {
         materialen = catalogus.geefMaterialen();
         Set<String> zoekTermen = new HashSet<>();
         zoekTermen.add("Lager onderwijs");
-        catalogus.filterMaterialen(MateriaalFilter.DOELGROEP,zoekTermen);
+        catalogus.filterMaterialen(MateriaalFilter.DOELGROEP, zoekTermen);
         Assert.assertEquals(1, materialen.size());
     }
-    
+
     @Test
-    public void testFilterMateriaalMetMeerdereZoektermGeeftAlleMaterialenTerugVanSoort(){
+    public void testFilterMateriaalMetMeerdereZoektermGeeftAlleMaterialenTerugVanSoort() {
         materialen = catalogus.geefMaterialen();
         Set<String> zoekTermen = new HashSet<>();
         zoekTermen.add("Lager onderwijs");
         zoekTermen.add("Secundair onderwijs");
-        catalogus.filterMaterialen(MateriaalFilter.DOELGROEP,zoekTermen);
+        catalogus.filterMaterialen(MateriaalFilter.DOELGROEP, zoekTermen);
         Assert.assertEquals(2, materialen.size());
     }
-    
-    
+
 }
